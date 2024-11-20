ce6caffb483319f60b19412ea1d126a1c9beac21
gorgeous2026 <ea8308010@gmail.com>
2024-11-20 22:10:42 +0530

    frst

---
+++ index.js
@@ -0,0 +1,11 @@
+//Step2: create express server
+const express = require('express');
+const bodyParser = require('body-parser');
+const studentRoutes = require('./routes/student');
+const app = express();
+const PORT = 3000;
+app.use(bodyParser.json()); // Parse JSON bodies
+app.use('/api/students', studentRoutes); // Use student routes
+app.listen(PORT, () => {
+console.log(`Server running on http://localhost:${PORT}`);
+});
